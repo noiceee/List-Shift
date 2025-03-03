cd1ef735e62b51f83cf76eb26f0f5af4555e53b6
Kartikey Singh <codingwalaid@gmail.com>
2025-03-03 21:26:48 +0530

    Updated package.json

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-export default List = ({
+export default function List ({
   listName,
   items,
   setItem,
   selectedItems,
   setSelectedItems,
-}) => {
+}) {
   // const [items, setItem] = useState(["Option1", "Option2", "Option3"]);
   // const [selectedItems, setSelectedItems] = useState([]);
 
@@ -54,7 +54,7 @@ export default List = ({
             }}
             checked={selectedItems?.length === 0}
           />
-          <label for={"select-all" + listName}>Select All</label>
+          <label htmlFor={"select-all" + listName}>Select All</label>
         </div>
         <button onClick={handleAdd}>Add</button>
         <div className="option-list">
